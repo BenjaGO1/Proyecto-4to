bf262702a83692f32f3a5edc91ecad111f615af3
GONZALEZ OLIVA BENJAMIN <101335426+BenjaGO1@users.noreply.github.com>
2022-07-14 17:32:59 -0300

    Camera

---
+++ Assets/CameraRotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+
+    public float speed;
+
+
+
+    
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
