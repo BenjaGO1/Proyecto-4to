07875c85fdef551d7cd464191d125383152924a4
BenjaGO1 <101335426+BenjaGO1@users.noreply.github.com>
2022-07-15 11:35:55 -0300

    Cambios

---
+++ Assets/scripts/Attack.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Attack : MonoBehaviour
+{
+    public bool activate = false;
+    public bool botones = true;
+    public GameObject Cuenta;
+    public GameObject Heal;
+    public GameObject Object;
+    public GameObject Ataque;
+
+
+    private void Update()
+    {
+        if (activate == false)
+        {
+            Cuenta.SetActive(false);
+        }
+        if(activate == true)
+        {
+            Cuenta.SetActive(true);
+        }
+        if (botones == false)
+        {
+            Heal.SetActive(false);
+            Object.SetActive(false);
+            Ataque.SetActive(false);
+        }
+        if (botones == true)
+        {
+            Heal.SetActive(true);
+            Object.SetActive(true);
+            Ataque.SetActive(true);
+        }
+    }
+    
+   
+    public void Activar()
+    {
+        activate = true;
+        botones = false;
+    }
+
+}
---
+++ Assets/scripts/Pausa.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pausa : MonoBehaviour
+{
+    public GameObject Pause;
+    public bool ayuda = false;
+    public bool ShowCursor;
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.P))
+        {
+            ayuda = true;
+        }
+        if (ayuda == true)
+        {
+            Pause.SetActive(true);
+            Cursor.visible = true;
+        }
+    }
+}
