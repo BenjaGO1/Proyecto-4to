66d78dc0b887dc5c5b319993d79048be7b8bd098
BenjaGO1 <101335426+BenjaGO1@users.noreply.github.com>
2022-08-02 12:01:13 -0300

    lucheta

--- Assets/scripts/CursorLookAt.cs
+++ Assets/scripts/CursorLookAt.cs
@@ -21,8 +21,7 @@ public class CursorLookAt : MonoBehaviour
     void Update()
     {
         float newRotationY = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * Sensitivity;
-        float newRotationX = transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * Sensitivity;
 
-        gameObject.transform.localEulerAngles = new Vector3(newRotationX, newRotationY, 0);
+        gameObject.transform.localEulerAngles = new Vector3(0, newRotationY, 0);
     }
 }
---
+++ Assets/scripts/Volver.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Volver : MonoBehaviour
+{
+    public bool activate = true;
+    public bool botones = false;
+    public GameObject inventario;
+    public GameObject Heal;
+    public GameObject Object;
+    public GameObject Ataque;
+    public GameObject Escape;
+    public GameObject Volver2;
+
+
+
+
+    void Update()
+    {
+        if (activate == true)
+        {
+            inventario.SetActive(true);
+        }
+
+        if (activate == false)
+        {
+            inventario.SetActive(false);
+        }
+        if (botones == false)
+        {
+            Heal.SetActive(false);
+            Object.SetActive(false);
+            Ataque.SetActive(false);
+            Escape.SetActive(false);
+            Volver2.SetActive(true);
+        }
+        if (botones == true)
+        {
+            Heal.SetActive(true);
+            Object.SetActive(true);
+            Ataque.SetActive(true);
+            Escape.SetActive(true);
+            Volver2.SetActive(false);
+        }
+    }
+
+    public void Activar()
+    {
+        activate = false;
+        botones = true;
+    }
+}
