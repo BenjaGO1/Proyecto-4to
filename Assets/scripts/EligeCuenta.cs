48f04d730c2daa9736f55603227a287842a14adc
BenjaGO1 <101335426+BenjaGO1@users.noreply.github.com>
2022-08-11 14:15:51 -0300

    pequeños cambios 11/8

--- Assets/scripts/EligeCuenta.cs
+++ Assets/scripts/EligeCuenta.cs
@@ -100,7 +100,7 @@ public class EligeCuenta : MonoBehaviour
 
         else
         {
-            Debug.Log("Respuesta incorrecta");
+            //Debug.Log("Respuesta incorrecta");
             string correcionando = ("Respuesta incorrecta");
             correccionando.text = correcionando;
         }
--- Assets/scripts/ShowPrompt.cs
+++ Assets/scripts/ShowPrompt.cs
@@ -7,15 +7,29 @@ public class ShowPrompt : MonoBehaviour
 {
 
     public Canvas EPromptCanvas;
+    public GameObject dialogo;
+    public GameObject Eprompt;
+    private bool trigger = false;
 
-    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E) && trigger == true)
+        {
+            dialogo.SetActive(true);
+            Eprompt.SetActive(false);
+        }
+    }
+
     void OnTriggerEnter(Collider ElTrigger)
     {
         if(ElTrigger.tag == "Player")
         {
             Debug.Log("Estas cerca del NPC");
             EPromptCanvas.enabled = true;
+            trigger = true;
+            dialogo.SetActive(false);
         }
+        
     }
     void OnTriggerExit(Collider SalirTrigger)
     {
