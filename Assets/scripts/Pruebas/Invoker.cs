dec871f56f0549d31cb97d417bdd9ab591d30876
BenjaGO1 <101335426+BenjaGO1@users.noreply.github.com>
2022-09-08 09:14:23 -0300

    base

---
+++ Assets/scripts/Pruebas/Invoker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invoker : MonoBehaviour
+{
+    public GameObject btn;
+    public GameObject TxT;
+    void Start()
+    {
+        InvokeRepeating("DesaparecerBoton", 5, 3);
+
+        CancelInvoke();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DesaparecerBoton()
+    {
+        btn.SetActive(false);
+        TxT.SetActive(true);
+    }
+}
