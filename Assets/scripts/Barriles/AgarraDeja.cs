c7778e495a8be196001297e3f6ffb23fcc0ebe79
Ren-Langlois <101406518+Ren-Langlois@users.noreply.github.com>
2022-09-08 14:26:15 -0300

    8/9 barriles

---
+++ Assets/scripts/Barriles/AgarraDeja.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgarraDeja : MonoBehaviour
+{
+    public GameObject barril;
+    public GameObject player;
+
+    float Yplayer;
+    float Xplayer;
+    float Zplayer;
+
+    float Ybarril;
+    float Xbarril;
+    float Zbarril;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ybarril = barril.transform.localPosition.y;
+        Xbarril = barril.transform.localPosition.x;
+        Zbarril = barril.transform.localPosition.z;
+    }
+  
+    // Update is called once per frame
+    void Update()
+    {
+        Yplayer = player.transform.localPosition.y;
+        Xplayer = player.transform.localPosition.x;
+        Zplayer = player.transform.localPosition.z;
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Barril")
+        {
+            //Ybarril = Yplayer;
+            //Xbarril = Xplayer;
+            //Zbarril = Zplayer;        
+        }
+
+        
+    }
+}
