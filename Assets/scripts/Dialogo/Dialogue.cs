14e5f66b80bb46b5110da7af6c30d216a9c77fa6
BenjaGO1 <101335426+BenjaGO1@users.noreply.github.com>
2022-10-25 12:06:40 -0300

    dialogo completamente funcional y muy fachero

---
+++ Assets/scripts/Dialogo/Dialogue.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+    public GameObject DialogueBox;
+    public Movement movimiento;
+    public CursorLookAt miralo;
+    public bool PasaronCosas;
+
+    private int index;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        StartDialogue();
+        PasaronCosas = false;
+}
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+        
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach(char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if(index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+
+        else
+        {
+            DialogueBox.SetActive(false);
+            textComponent.enabled = false;
+            movimiento.WalkSpeed = 0.3f;
+            movimiento.RunSpeed = 0.6f;
+            movimiento.jumpForce = 6;
+            miralo.Sensitivity = 8;
+            PasaronCosas = true;
+        }
+    }
+}
---
+++ Assets/scripts/Dialogo/DialogueTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public GameObject dialogobox;
+    public Movement movimiento;
+    public CursorLookAt miralo;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider Other)
+    {
+        if (Other.gameObject.tag == "Player")
+        {
+            dialogobox.SetActive(true);
+            movimiento.WalkSpeed = 0;
+            movimiento.RunSpeed = 0;
+            movimiento.jumpForce = 0;
+            miralo.Sensitivity = 0;
+        }
+
+    }
+}
