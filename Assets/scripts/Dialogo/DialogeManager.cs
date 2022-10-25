7fd8d2a2e5faffaab6f5770eec3542bd651cf940
BenjaGO1 <101335426+BenjaGO1@users.noreply.github.com>
2022-10-25 10:53:28 -0300

    no c

---
+++ Assets/scripts/Dialogo/DialogeManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogeManager : MonoBehaviour
+{
+    [SerializeField] GameObject dialogueUI;
+    public Text textoDelDialogo;
+    public GameObject fondo;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("NPC"))
+        {
+            
+            dialogueUI.SetActive(true);
+
+            if (Input.GetKey(KeyCode.E))
+            {
+                textoDelDialogo.text = "Hola viajero";
+            }
+        }
+        
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("NPC"))
+        {
+            dialogueUI.SetActive(false);
+        }
+    }
+}
